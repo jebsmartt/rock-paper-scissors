04d1d4305690c44a8bbd8a5b7aad7679c9142967
jebsmartt <jeb.smartt@gmail.com>
2023-07-10 22:12:47 -0700

    Allow user to play 1 game of Rock, Paper, Scissors

---
+++ rock_paper_scissors.js
@@ -0,0 +1,66 @@
+const options = ["rock", "paper", "scissors"]
+
+function titleCase(string) {
+    let sentence = string.toLowerCase().split(" ")
+    for(let i = 0; i< sentence.length; i++) {
+        sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1)
+    }
+    return sentence.join(" ")
+}
+
+// Picks a play for the AI
+function getComputerChoice() {
+    let randomIndex = Math.floor(Math.random() * options.length)
+    const computerChoice = options[randomIndex]
+    
+    return computerChoice
+}
+
+// Declares a winner
+function winOrLose(win, userSelection, computerSelection) {
+    if (win === true) {
+        console.log(`You win! ${titleCase(userSelection)} beats ${titleCase(computerSelection)}`)
+    } else if (win === false) {
+        console.log(`You lose! ${titleCase(computerSelection)} beats ${titleCase(userSelection)}`)
+    }
+}
+
+// Determines a winner
+function playRound() {
+    const userSelection = prompt("Choose Rock, Paper, or Scissors: ").toLowerCase()
+    const computerSelection = getComputerChoice()
+
+    if (userSelection === 'rock' || userSelection === 'paper' || userSelection === 'scissors') {
+        switch (userSelection) {
+            case computerSelection:
+                console.log(`It was a Tie! ${titleCase(userSelection)} ties ${titleCase(computerSelection)}`)
+                break
+
+            case 'rock':
+                if (computerSelection === 'scissors') {
+                    winOrLose(true, userSelection,computerSelection)
+                } else {
+                    winOrLose(false,userSelection, computerSelection)
+                }
+                break
+
+            case 'scissors':
+                if (computerSelection === 'paper') {
+                    winOrLose(true, userSelection,computerSelection)
+                } else {
+                    winOrLose(false,userSelection, computerSelection)
+                }
+                break
+
+            case 'paper':
+                if (computerSelection === 'rock') {
+                    winOrLose(true, userSelection,computerSelection)
+                } else {
+                    winOrLose(false,userSelection, computerSelection)
+                }
+                break
+        }
+    } else {
+        console.log('Error: Only enter Rock, Paper, or Scissors!')
+    }
+}
