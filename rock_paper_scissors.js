ca13b41eec23ee06d3ce402753346011810c1491
jebsmartt <jeb.smartt@gmail.com>
2023-07-12 21:53:00 -0700

    Refactor display of game result Changed behavior to have the event listener update the text of an existing div instead of generating a new div. Also, adjusted behavior of winOrLose to handle case of tie to extract the responsibility of generating the result message from the playRound function. winOrLose now returns an object.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -17,11 +17,23 @@ function getComputerChoice() {
 }
 
 // Declares a winner
-function winOrLose(win, userSelection, computerSelection) {
-    if (win === true) {
-        return `You win! ${titleCase(userSelection)} beats ${titleCase(computerSelection)}`
-    } else if (win === false) {
-        return `You lose! ${titleCase(computerSelection)} beats ${titleCase(userSelection)}`
+function winOrLose(winner, userSelection, computerSelection) {
+    if (winner === 'player') {
+        return {
+            result: 'win',
+            message: `You win! ${titleCase(userSelection)} beats ${titleCase(computerSelection)}`,
+        }
+
+    } else if (winner === 'computer') {
+        return {
+            result: 'loss',
+            message: `You lose! ${titleCase(computerSelection)} beats ${titleCase(userSelection)}`,
+        }
+    } else {
+        return {
+            result: 'tie',
+            message: `It was a Tie! ${titleCase(userSelection)} ties ${titleCase(computerSelection)}`,
+        }
     }
 }
 
@@ -30,68 +42,65 @@ function playRound(userSelection) {
     // let userSelection = prompt("Choose Rock, Paper, or Scissors: ").toLowerCase()
     let computerSelection = getComputerChoice()
 
+    // Check if userSelection is valid
     if (userSelection === 'rock' || userSelection === 'paper' || userSelection === 'scissors') {
         switch (userSelection) {
+            // Tie Case
             case computerSelection:
-                return `It was a Tie! ${titleCase(userSelection)} ties ${titleCase(computerSelection)}`
-
+                return winOrLose('tie',userSelection, computerSelection)
+            // Win or Loss Cases
             case 'rock':
                 if (computerSelection === 'scissors') {
-                    return winOrLose(true, userSelection,computerSelection)
+                    return winOrLose('player', userSelection,computerSelection)
                 } else {
-                    return winOrLose(false,userSelection, computerSelection)
+                    return winOrLose('computer',userSelection, computerSelection)
                 }
 
             case 'scissors':
                 if (computerSelection === 'paper') {
-                    return winOrLose(true, userSelection,computerSelection)
+                    return winOrLose('player', userSelection,computerSelection)
                 } else {
-                    return winOrLose(false,userSelection, computerSelection)
+                    return winOrLose('computer',userSelection, computerSelection)
                 }
 
             case 'paper':
                 if (computerSelection === 'rock') {
-                    return winOrLose(true, userSelection,computerSelection)
+                    return winOrLose('player', userSelection,computerSelection)
                 } else {
-                    return winOrLose(false,userSelection, computerSelection)
+                    return winOrLose('computer',userSelection, computerSelection)
                 }
         }
     } else {
-        return 'Error: Only enter Rock, Paper, or Scissors!'
-    }
-}
-
-// Allow for multiple iterations of games
-function game(numberOfGames) {
-    let i = 0
-    while (i < numberOfGames) {
-        i++
-        console.log(playRound())
+        // If userSelection not valid, return error
+        return alert('Error: Only enter Rock, Paper, or Scissors!')
     }
 }
 
 function displayResults(userChoice) {
     // Add a div to the page to display the log messages to track what is happening
-    let existingDiv = document.getElementById('resultsDiv')
-    
-    if (!existingDiv) {
-        // 1. Use createElement to create a new div
-        const resultsDiv = document.createElement('div');
-        // 2. Determine the target for your new element
-        const body = document.body;
-        // 3. Optionally customize the created new div before adding it to target
-        resultsDiv.id = 'resultsDiv';
-        resultsDiv.style.background = 'lightgrey'
-        resultsDiv.style.border = '2px solid black'
-        resultsDiv.style.marginTop = '20px'
-        resultsDiv.style.padding = '15px'
-        resultsDiv.textContent = playRound(userChoice)
+    let existingDiv = document.getElementById('results-bar')
 
-    // 4. Add element to target
-        body.appendChild(resultsDiv);
-    } else {
-        existingDiv.textContent = playRound(userChoice);
-    };
+    let result = playRound(userChoice)
+    existingDiv.textContent = result.message;
+
+    // if (!existingDiv) {
+    //     // 1. Use createElement to create a new div
+    //     const resultsDiv = document.createElement('div');
+    //     // 2. Determine the target for your new element
+    //     const body = document.body;
+    //     // 3. Optionally customize the created new div before adding it to target
+    //     resultsDiv.id = 'resultsDiv';
+    //     resultsDiv.style.background = 'lightgrey'
+    //     resultsDiv.style.border = '2px solid black'
+    //     resultsDiv.style.marginTop = '20px'
+    //     resultsDiv.style.padding = '15px'
+    //     resultsDiv.textContent = playRound(userChoice)
+
+    // // 4. Add element to target
+    //     body.appendChild(resultsDiv);
+    // } else {
+    //     existingDiv.textContent = playRound(userChoice);
+    // };
 }
 
 
@@ -107,3 +116,9 @@ playerSelectionButtons.forEach(function(button) {
     });
 });
 
+
+// Left off -- Display the running score, and announce a winner of the game once one player reaches 5 points.
+// I need a counter for each player
+// I need to identify if one of the players has reached 5
+// I need a way to display the count
+// I need a way to display the winner
